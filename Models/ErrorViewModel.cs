61c6e811c6f8c253b5dee64a2ab5fb228d55aa65
cohavcahana <cohavca@ac.sce.ac.il>
2025-11-22 12:21:43 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TravelAgencyProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
