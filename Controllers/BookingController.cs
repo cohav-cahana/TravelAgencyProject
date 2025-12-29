c703b34d189c22fb4962ea63cf3ff351d9d0d77e
mariabadarne <mariaba@ac.sce.ac.il>
2025-12-29 16:33:53 +0200

    my booking area

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelAgencyProject.Data;
+using TravelAgencyProject.Models;
 
 namespace TravelAgencyProject.Controllers
 {
     public class BookingController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+        public BookingController(AppDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userIdString = HttpContext.Session.GetString("UserId");
+            if (string.IsNullOrEmpty(userIdString))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            int userId = int.Parse(userIdString);
+
+            // שליפת כל ההזמנות השייכות למשתמש הזה, כולל פרטי הטיול
+            var myBookings = await _context.Bookings
+                .Include(b => b.Trip)
+                .Where(b => b.UserId == userId)
+                .OrderByDescending(b => b.BookingDate)
+                .ToListAsync();
+
+            return View(myBookings);
         }
     }
 }
